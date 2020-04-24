07c919529ae46e864bdbcbe77f7e285a56e994ca
ho3ein-mola <hosein.metro@gmail.com>
2020-04-24 15:45:19 +0430

    dev bug fix

--- electron/main.ts
+++ electron/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain,  } from 'electron';
+import { app, BrowserWindow, ipcMain, } from 'electron';
 import * as path from 'path';
 import * as isDev from 'electron-is-dev';
 import installExtension, { REACT_DEVELOPER_TOOLS } from "electron-devtools-installer";
@@ -14,58 +14,40 @@ let _setting: Setting;
 let _filemanager: FileManager;
 let _path: Path;
 
-function createWindow() {
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
+const createWindow = () => {
+    win = new BrowserWindow({
+        width: 1280,
+        height: 760,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    if (isDev) {
+        win.loadURL('http://localhost:3000/index.html');
+    } else {
+        // 'build/index.html'
+        win.loadURL(`file://${__dirname}/../index.html`);
     }
-  })
 
-  if (isDev) {
-    win.loadURL('http://localhost:3000/index.html');
-  } else {
-    // 'build/index.html'
-    win.loadURL(`file://${__dirname}/../index.html`);
-  }
+    win.on('closed', () => win = null);
+    // Hot Reloading
+    if (isDev) {
+        // 'node_modules/.bin/electronPath'
+        require('electron-reload')(__dirname, {
+            electron: path.join(__dirname, '..', '..', 'node_modules', '.bin', 'electron'),
+            forceHardReset: true,
+            hardResetMethod: 'exit'
+        });
+    }
+    //   DevTools
+    installExtension(REACT_DEVELOPER_TOOLS)
+        .then((name) => console.log(`Added Extension:  ${name}`))
+        .catch((err) => console.log('An error occurred: ', err));
 
-  win.on('closed', () => win = null);
-
-  // Hot Reloading
-  if (isDev) {
-    // 'node_modules/.bin/electronPath'
-    require('electron-reload')(__dirname, {
-      electron: path.join(__dirname, '..', '..', 'node_modules', '.bin', 'electron'),
-      forceHardReset: true,
-      hardResetMethod: 'exit'
-    });
-  }
-
-  // DevTools
-  installExtension(REACT_DEVELOPER_TOOLS)
-    .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log('An error occurred: ', err));
-
-  if (isDev) {
-    win.webContents.openDevTools();
-  }
+    if (isDev) {
+        win.webContents.openDevTools();
+    }
 }
-
-app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  if (win === null) {
-    createWindow();
-  }
-});
-
 
 app.on('ready', () => {
     createWindow();
@@ -86,7 +68,6 @@ app.on('activate', () => {
         createWindow();
     }
 });
-
 const setProjectPath = (event: any, projectPath: any) => {
     _setting.readSetting().then((data: string) => {
         let _projectPath;
@@ -169,6 +150,7 @@ ipcMain.on('explorer:changeFolderAndUpdateList', (event, data) => {
 });
 
 ipcMain.on('explorer:changeFolderBackAndUpdateList', (event, data) => {
+    log.info('test4');
     if (data.projectPath !== '' && data.projectPath !== undefined) {
         if (data.projectPath !== '' && data.projectPath !== undefined) {
             const normal = [_path.normilizePath(data.projectPath)];
