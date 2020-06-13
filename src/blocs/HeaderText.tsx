a46c7298c2dbeb5a26eec84d16f45dffab5d4d2e
ho3ein-mola <hosein.metro@gmail.com>
2020-06-13 11:02:24 +0430

    add color function

--- src/blocs/HeaderText.tsx
+++ src/blocs/HeaderText.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { Editor, EditorState, RichUtils, convertToRaw, convertFromRaw, CompositeDecorator, ContentBlock } from 'draft-js';
+import { Editor, EditorState, RichUtils, convertToRaw, convertFromRaw, CompositeDecorator, ContentBlock, Modifier } from 'draft-js';
+import _ from 'lodash';
 import Immutable from 'immutable';
 import ReactDOM from 'react-dom';
 import * as uuid from 'uuid';
@@ -100,6 +101,8 @@ const HeaderText = (props: any) => {
     }
     return "";
   }
+
+
   useEffect(() => {
     var init;
     if (initState && initState == '') {
@@ -149,28 +152,125 @@ const HeaderText = (props: any) => {
     setShowControll(false)
   }
 
+  const clearInlineStyles = (editorState: any, styles: any) => {
+    const contentWithoutStyles = _.reduce(styles, (newContentState, style) => (
+      Modifier.removeInlineStyle(
+        newContentState,
+        editorState.getSelection(),
+        style
+      )
+    ), editorState.getCurrentContent());
+    return EditorState.push(
+      editorState,
+      contentWithoutStyles,
+      'change-inline-style'
+    );
+  };
+
+
+  const _toggle_color = (color: string) => {
+    const inlineStyle = editorState.getCurrentInlineStyle();
+    const selection = editorState.getSelection();
+
+    // Let's just allow one color at a time. Turn off all active colors.
+    const nextContentState = Object.keys(colorStyleMap)
+      .reduce((contentState, color) => {
+        return Modifier.removeInlineStyle(contentState, selection, color)
+      }, editorState.getCurrentContent());
+
+    let nextEditorState = EditorState.push(
+      editorState,
+      nextContentState,
+      'change-inline-style'
+    );
+    const currentStyle = editorState.getCurrentInlineStyle();
+    // Unset style override for current color.
+    if (selection.isCollapsed()) {
+      nextEditorState = currentStyle.reduce((state: any, color: any) => {
+        return RichUtils.toggleInlineStyle(state, color);
+      }, nextEditorState);
+    }
+    // If the color is being toggled on, apply it.
+    if (!currentStyle.has(color)) {
+      nextEditorState = RichUtils.toggleInlineStyle(
+        nextEditorState,
+        color
+      );
+    }
+    update(nextEditorState);
+  }
+
+  var COLORS = [
+    { label: 'Red', style: 'red' },
+    { label: 'Orange', style: 'orange' },
+    { label: 'Yellow', style: 'yellow' },
+    { label: 'Green', style: 'green' },
+    { label: 'Blue', style: 'blue' },
+    { label: 'Indigo', style: 'indigo' },
+    { label: 'Violet', style: 'violet' },
+    { label: 'size', style: 'size' },
+  ];
+
+  const colorStyleMap = {
+    red: {
+      color: 'rgba(255, 0, 0, 1.0)',
+    },
+    orange: {
+      color: 'rgba(255, 127, 0, 1.0)',
+    },
+    yellow: {
+      color: 'rgba(180, 180, 0, 1.0)',
+    },
+    green: {
+      color: 'rgba(0, 180, 0, 1.0)',
+    },
+    blue: {
+      color: 'rgba(0, 0, 255, 1.0)',
+    },
+    indigo: {
+      color: 'rgba(75, 0, 130, 1.0)',
+    },
+    violet: {
+      color: 'rgba(127, 0, 255, 1.0)',
+    },
+    size: {
+      fontSize: "100px",
+    },
+  };
+
+
+  const renderColors = () => {
+    return COLORS.map((color, index) => {
+      return (
+        <button onClick={(e: any) => _toggle_color(color.style)} key={index} className="h-10 w-16 border">{color.label}</button>
+      )
+    });
+  }
+
   return (
-    <div className="" onClick={focus}>
-      <div className={(showControll) ? " sticky top-0 w-full border z-40 bg-white " : "hidden"}>
-        <button onClick={() => _toggleBlocStyle('ITALIC')} className="h-8 w-16 border">I</button>
-        <button onClick={() => _toggleBlocStyle('BOLD')} className="h-8 w-16 border">B</button>
-        <button onClick={() => _toggleBlocStyle('UNDERLINE')} className="h-8 w-16 border">U</button>
-        <button onClick={() => _toggleBlocStyle('CODE')} className="h-8 w-16 border">C</button>
-        <button onClick={() => _toggleBlockType('header-one')} className="h-8 w-16 border">H1</button>
-        <button onClick={() => _toggleBlockType('header-two')} className="h-8 w-16 border">H2</button>
-        <button onClick={() => _toggleBlockType('header-three')} className="h-8 w-16 border">H3</button>
-        <button onClick={() => _toggleBlockType('header-four')} className="h-8 w-16 border">H4</button>
-        <button onClick={() => _toggleBlockType('header-five')} className="h-8 w-16 border">H5</button>
-        <button className="h-8 w-16 border">OL</button>
-        <button className="h-8 w-16 border">UL</button>
+    <div className="" onMouseEnter={focus} onClick={focus} onMouseLeave={blur}>
+      <div className={(showControll) ? " sticky top-0 w-full border border-b-0 border-t-0 z-40 bg-white " : "hidden"}>
+        <button onClick={() => _toggleBlocStyle('ITALIC')} className="h-10 w-16 border">I</button>
+        <button onClick={() => _toggleBlocStyle('BOLD')} className="h-10 w-16 border">B</button>
+        <button onClick={() => _toggleBlocStyle('UNDERLINE')} className="h-10 w-16 border">U</button>
+        <button onClick={() => _toggleBlocStyle('CODE')} className="h-10 w-16 border">C</button>
+        <button onClick={() => _toggleBlockType('header-one')} className="h-10 w-16 border">H1</button>
+        <button onClick={() => _toggleBlockType('header-two')} className="h-10 w-16 border">H2</button>
+        <button onClick={() => _toggleBlockType('header-three')} className="h-10 w-16 border">H3</button>
+        <button onClick={() => _toggleBlockType('header-four')} className="h-10 w-16 border">H4</button>
+        <button onClick={() => _toggleBlockType('header-five')} className="h-10 w-16 border">H5</button>
+        <button className="h-10 w-16 border">OL</button>
+        <button className="h-10 w-16 border">UL</button>
+        {renderColors()}
       </div>
       <Editor
         editorState={editorState}
+        blockRenderMap={blockRenderMap}
         blockStyleFn={myBlockStyleFn}
+        customStyleMap={colorStyleMap}
         placeholder="enter something"
         onChange={(e: any) => handleChange(e)}
         handleKeyCommand={handleKeyCommand}
-        blockRenderMap={blockRenderMap}
         spellCheck={true}
       />
     </div >
