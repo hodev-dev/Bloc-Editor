7251b536c60f891a14c6e8cf78e4bd1576363b1d
ho3ein-mola <hosein.metro@gmail.com>
2020-07-17 23:38:33 +0430

    some changes

--- electron/utail/Path.ts
+++ electron/utail/Path.ts
@@ -1,19 +1,19 @@
-import {app} from 'electron';
+import { app } from 'electron';
 import * as path from 'path';
-import * as dotenv  from 'dotenv';
+import * as dotenv from 'dotenv';
 const env = dotenv.config();
 
 class Path {
-   getConfigFolderPath(): string{
-      return path.join(app.getPath('appData'),"Notilda-Config");
+   getConfigFolderPath(): string {
+      return path.join(app.getPath('appData'), "Bloc_Editor");
    }
-   getConfigFilePath(): string{
-    let configfolder: string = this.getConfigFolderPath();
-    return path.join(configfolder,'setting.json');
+   getConfigFilePath(): string {
+      let configfolder: string = this.getConfigFolderPath();
+      return path.join(configfolder, 'setting.json');
    }
-   normilizePath(path: string){
-      return path.substring(1,path.length - 1);
-  }
+   normilizePath(path: string) {
+      return path.substring(1, path.length - 1);
+   }
 }
 
 export default Path
